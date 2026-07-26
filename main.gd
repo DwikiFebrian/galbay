faeff65c8e14bd7eee6afa47fc38187f9fb49d0a
WLan1707 <muhammadwlan@gmail.com>
2026-07-26 19:16:25 +0700

    upggrades

--- main.gd
+++ main.gd
@@ -7,6 +7,14 @@ extends Node2D
 @onready var label_queue = $"Control/Main layout/Panel atas/queue"
 @onready var container_draft = $"Control/Main layout/PanelContainer/Paneh tengah/Targetlist"
 @onready var container_aktif = $"Control/Main layout/list/VBoxContainer/Paneh bawah/PanelContainer/list nasabah"
+@onready var middle_sucker_button = $"Control/Main layout/list/upgrade/MarginContainer/list upgrade/middle sucker/middle sucker"
+@onready var high_sucker_button = $"Control/Main layout/list/upgrade/MarginContainer/list upgrade/high sucker/high sucker"
+@onready var counter_expansion_button = $"Control/Main layout/list/upgrade/MarginContainer/list upgrade/counter expansion/button"
+@onready var doomer_influencer_button = $"Control/Main layout/list/upgrade/MarginContainer/list upgrade/doomer influencer/button"
+@onready var broken_kneecaps_button = $"Control/Main layout/list/upgrade/MarginContainer/list upgrade/broken kneecaps/button"
+@onready var middle_sucker_upgrades = $"Control/Main layout/list/upgrade/MarginContainer/list upgrade/middle sucker"
+@onready var high_sucker_upgrades = $"Control/Main layout/list/upgrade/MarginContainer/list upgrade/high sucker"
+@onready var broken_kneecaps_upgrades = $"Control/Main layout/list/upgrade/MarginContainer/list upgrade/broken kneecap"
 
 var nasabah_scene = preload("res://nasabahcards.tscn")
 var active_row_scene = preload("res://nasabahaktif.tscn")
@@ -17,7 +25,7 @@ var current_turn: int = 0
 var deadline: int = 12
 var quarter: int = 0
 var max_turn: int = 48
-var target = [200, 500, 1000, 2000]
+var target = [200, 400, 900, 1500]
 var boss_goal: int = target[0]
 var nasabah_aktif: Array = []
 var antrean_nasabah: Array = [] # buat nyimpen 12 calon nasabah per turn
@@ -37,6 +45,18 @@ var harga_up2: int = 50
 var harga_up3: int = 50
 var harga_up4: int = 50
 var harga_up5: int = 50
+var harga_counter_expansion = [40, 75]
+var harga_middle_sucker = [50, 75, 100]
+var harga_high_sucker = [100, 200, 300]
+var harga_doomer_influencer = [30, 67, 100, 200]
+var harga_broken_kneecap = [75, 140]
+
+# level upgrade
+var level_middle_sucker = 0
+var level_high_sucker = 0
+var level_counter_expansion_sucker = 0
+var level_doomer_influencer = 0
+var level_broken_kneecaps = 0
 
 # katalog manual
 var template_kelas = [
@@ -45,7 +65,7 @@ var template_kelas = [
 		"modal_min": 10, "modal_max": 25,
 		"bunga_min": 0.15, "bunga_max": 0.25,
 		"return_min": 4, "return_max": 7,
-		"galbay_min": 0.07, "galbay_max": 0.15,
+		"galbay_min": 0.10, "galbay_max": 0.20,
 		"gambar": [
 			"res://sprite/low1.png", 
 			"res://sprite/low2.png", 
@@ -62,7 +82,7 @@ var template_kelas = [
 		"modal_min": 40, "modal_max": 80,
 		"return_min": 10, "return_max": 18,
 		"bunga_min": 0.10, "bunga_max": 0.30,
-		"galbay_min": 0.05, "galbay_max": 0.10,
+		"galbay_min": 0.07, "galbay_max": 0.15,
 		"gambar": [
 			"res://sprite/middle1.png",
 			"res://sprite/middle2.png",
@@ -76,7 +96,7 @@ var template_kelas = [
 		"modal_min": 100, "modal_max": 200,
 		"return_min": 25, "return_max": 40,
 		"bunga_min": 0.10, "bunga_max": 0.35,
-		"galbay_min": 0.01, "galbay_max": 0.05,
+		"galbay_min": 0.05, "galbay_max": 0.10,
 		"gambar": [
 			"res://sprite/high1.png",
 			"res://sprite/high2.png",
@@ -120,7 +140,7 @@ func generate_antrean_turn(jumlah: int) -> void:
 			"modal": randi_range(kelas_terpilih["modal_min"], kelas_terpilih["modal_max"]),
 			"bunga": randf_range(kelas_terpilih["bunga_min"], kelas_terpilih["bunga_max"]),
 			#"cicilan_per_turn": randi_range(kelas_terpilih["return_min"], kelas_terpilih["return_max"]),
-			"peluang_galbay": max(0.01, randf_range(kelas_terpilih["galbay_min"], kelas_terpilih["galbay_max"]) - reduksi_galbay),
+			"peluang_galbay": max(0.01, randf_range(kelas_terpilih["galbay_min"], kelas_terpilih["galbay_max"]) /pow(2, level_broken_kneecaps) ),
 			"tenor": 4 + bonus_tenor,
 			"path_gambar": kelas_terpilih["gambar"].pick_random()
 		}
@@ -165,7 +185,11 @@ func proses_terima_nasabah(kartu: Node, data: Dictionary) -> void:
 		
 		var baris_baru = active_row_scene.instantiate()
 		container_aktif.add_child(baris_baru)
-		baris_baru.isi_data(data) 
+		
+		if quarter <= 1:
+			baris_baru.isi_data_no_galbay(data)
+		else:
+			baris_baru.isi_data(data) 
 		
 		kartu.queue_free()
 		
@@ -185,7 +209,10 @@ func _on_nextweek_pressed() -> void:
 		#var kartu_baru = active_row_scene.instantiate()
 		#player_cash += nasabah["cicilan_per_turn"]
 		#player_cash += nasabah.data["cicilan_per_turn"]
-		nasabah.kurangi_turn()
+		if quarter <= 1:
+			nasabah.kurangi_turn_no_galbay()
+		else:
+			nasabah.kurangi_turn()
 	
 	for i in range(len(nasabah_aktif) - 1, -1, -1):
 		var nasabah = nasabah_aktif[i]
@@ -204,7 +231,6 @@ func _on_nextweek_pressed() -> void:
 		setor_boss()
 		current_turn = 0
 		quarter += 1
-		boss_goal = target[quarter]
 		new_quarter()
 	
 	update_ui_header()
@@ -217,56 +243,102 @@ func setor_boss():
 		player_cash -= boss_goal
 		
 func new_quarter():
+	if quarter == 4:
+		end_game()
+	else:
+		boss_goal = target[quarter]
+		if quarter == 1:
+			middle_sucker_upgrades.visible = true
+			high_sucker_upgrades.visible = true
+		elif quarter == 2:
+			broken_kneecaps_upgrades.visible = true
+		
+func end_game():
 	pass
 
 # tombol upgrade
 
-func _on_tombolbeli_1_pressed() -> void:
-	if player_cash >= harga_up1:
-		player_cash -= harga_up1
+func _on_middle_sucker_pressed() -> void:
+	if player_cash >= harga_middle_sucker[level_middle_sucker]:
+		player_cash -= harga_middle_sucker[level_middle_sucker]
+		weight[1] += 4
+		level_middle_sucker += 1
+		update_ui_header()
+		middle_sucker_button.text ="$" +  str(harga_middle_sucker[level_middle_sucker])
+	else:
+		print("Duit kurang buat upgrade Middle Sucker")
+	if level_middle_sucker == 2:
+		middle_sucker_button.disabled = true
+		
+func _on_high_sucker_pressed() -> void:
+	if player_cash >= harga_high_sucker[level_high_sucker]:
+		player_cash -= harga_high_sucker[level_high_sucker]
+		weight[1] += 5
+		level_high_sucker += 1
+		update_ui_header()
+		high_sucker_button.text ="$" +  str(harga_high_sucker[level_high_sucker])
+	else:
+		print("Duit kurang buat upgrade High Sucker")
+	if level_middle_sucker == 3:
+		middle_sucker_button.disabled = true
+
+func _on_counter_expansion_pressed() -> void:
+	if player_cash >= harga_counter_expansion[level_counter_expansion_sucker]:
+		player_cash -= harga_counter_expansion[level_counter_expansion_sucker]
 		max_tampil += 1 
-		harga_up1 += 30 
+		level_counter_expansion_sucker += 1 
+		
 		update_ui_header()
 		isi_slot_kosong() 
+		
+		counter_expansion_button.text = "$" + str(harga_counter_expansion[level_counter_expansion_sucker])
 	else:
-		print("Duit kurang buat Upgrade 1!")
+		print("Duit kurang buat upgrade Counter Expansion!")
+	
+	if level_counter_expansion_sucker == 2:
+		counter_expansion_button.disabled = true
 
 
-func _on_tombolbeli_2_pressed() -> void:
-	if player_cash >= harga_up2:
-		player_cash -= harga_up2
-		bonus_return += 1
-		harga_up2 += 50
-		update_ui_header()
-	else:
-		print("Duit kurang buat Upgrade 2!")
+#func _on_tombolbeli_2_pressed() -> void:
+	#if player_cash >= harga_up2:
+		#player_cash -= harga_up2
+		#bonus_return += 1
+		#harga_up2 += 50
+		#update_ui_header()
+	#else:
+		#print("Duit kurang buat Upgrade 2!")
 
 
-func _on_tombolbeli_3_pressed() -> void:
-	if player_cash >= harga_up3:
-		player_cash -= harga_up3
+func _on_doomer_influencer_pressed() -> void:
+	if player_cash >= harga_doomer_influencer[level_doomer_influencer]:
+		player_cash -= harga_doomer_influencer[level_doomer_influencer]
 		queue_length += 2
-		harga_up3 += 50
+		level_doomer_influencer += 1
 		update_ui_header()
 	else:
-		print("Duit kurang buat Upgrade 3!")
+		print("Duit kurang buat upgrade Doomer Influencer!")
+		
+	if level_doomer_influencer == 4:
+		doomer_influencer_button.disabled = true
+		
 
 
-func _on_tombolbeli_4_pressed() -> void:
-	if player_cash >= harga_up4:
-		player_cash -= harga_up4
-		bonus_tenor += 1
-		harga_up4 += 50
-		update_ui_header()
-	else:
-		print("Duit kurang buat Upgrade 4!")
+#func _on_tombolbeli_4_pressed() -> void:
+	#if player_cash >= harga_up4:
+		#player_cash -= harga_up4
+		#bonus_tenor += 1
+		#harga_up4 += 50
+		#update_ui_header()
+	#else:
+		#print("Duit kurang buat Upgrade 4!")
 
 
-func _on_tombolbeli_5_pressed() -> void:
-	if player_cash >= harga_up5:
-		player_cash -= harga_up5
-		reduksi_galbay += 0.03
-		harga_up5 += 50
+func _on_broken_kneecap_pressed() -> void:
+	if player_cash >= harga_broken_kneecap[level_broken_kneecaps]:
+		player_cash -= harga_broken_kneecap[level_broken_kneecaps]
+		level_broken_kneecaps += 1
 		update_ui_header()
 	else:
 		print("Duit kurang buat Upgrade 5!")
+	if level_broken_kneecaps == 2:
+		broken_kneecaps_button.disabled = true
--- nasabahaktif.gd
+++ nasabahaktif.gd
@@ -3,7 +3,7 @@ extends HBoxContainer
 @onready var avatar = $avatar
 @onready var label_nama = $infokiri/nama
 @onready var label_return = $infokiri/return
-@onready var label_turn = $turn
+@onready var label_turn = $infokanan/turn
 @onready var label_peluang_galbay = $infokiri/galbay
 
 var sisa_tenor: int = 4 
@@ -26,6 +26,22 @@ func isi_data(data: Dictionary) -> void:
 	label_nama.text = "ID: " + str(data["id_nasabah"]) + " | " + str(data["tipe_kelas"])
 	label_return.text = "Payback: +$" + str(data["cicilan_per_turn"]) + "/turn"
 	label_peluang_galbay.text = "Dodging probability: " + str(tampil_peluang_galbay) + " %"
+	
+	if data.has("tenor"):
+		sisa_tenor = data["tenor"]
+		
+	update_teks_turn()
+
+func isi_data_no_galbay(data: Dictionary) -> void:
+	data_nasabah = data
+	status_galbay = false
+	
+	if data.has("path_gambar"):
+		avatar.texture = load(data["path_gambar"])
+		
+	label_nama.text = "ID: " + str(data["id_nasabah"]) + " | " + str(data["tipe_kelas"])
+	label_return.text = "Payback: +$" + str(data["cicilan_per_turn"]) + "/turn"
+	#label_peluang_galbay.text = "Dodging probability: " + str(tampil_peluang_galbay) + " %"
 	
 	if data.has("tenor"):
 		sisa_tenor = data["tenor"]
@@ -55,6 +71,27 @@ func kurangi_turn() -> void:
 		label_return.add_theme_color_override("font_color", Color(1, 0, 0))
 		update_teks_turn()
 		return 
+	
+	sisa_tenor -= 1
+	update_teks_turn()
+	if sisa_tenor <= 0:
+		queue_free() 
+		
+func kurangi_turn_no_galbay() -> void:
+	if status_galbay:
+		queue_free() 
+		return
+		
+	#var roll_dadu = randf() 
+	#
+	#if roll_dadu <= data_nasabah["peluang_galbay"]:
+		#status_galbay = true
+		#data_nasabah["is_galbay"] = true 
+		#
+		#label_return.text = "GALBAY! ($0)"
+		#label_return.add_theme_color_override("font_color", Color(1, 0, 0))
+		#update_teks_turn()
+		#return 
 	
 	sisa_tenor -= 1
 	update_teks_turn()
